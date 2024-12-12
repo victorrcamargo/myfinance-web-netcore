4eb15e1a79cfc91c4a667612cdad02160234f4c3
victor <victorrcamargo@gmail.com>
2024-12-12 10:29:04 -0300

    Removendo projeto anterior

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace myfinance_web_netcore.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
